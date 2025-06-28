640cb14475ab1a60ba624484e835e77a96237358
EpsithO <matheodu25@gmail.com>
2025-06-28 23:10:25 +0200

    nouveau

--- api/src/main/java/com/CESIZen/prod/dto/tracker/EmotionTrackerResponseDto.java
+++ api/src/main/java/com/CESIZen/prod/dto/tracker/EmotionTrackerResponseDto.java
@@ -1,51 +1,51 @@
 package com.CESIZen.prod.dto.tracker;
 
+import java.time.LocalDate;
+
 public class EmotionTrackerResponseDto {
-
     private Long id;
-    private String emotion;
+    private String emotionName;
     private String description;
-    private String date;
+    private LocalDate date;
 
-    public EmotionTrackerResponseDto() {
-    }
-
-    public EmotionTrackerResponseDto(Long id, String emotion, String description, String date) {
+    public EmotionTrackerResponseDto(Long id, String emotionName, String description, LocalDate date) {
         this.id = id;
-        this.emotion = emotion;
+        this.emotionName = emotionName;
         this.description = description;
         this.date = date;
     }
 
+    // Getters
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmotion() {
-        return emotion;
-    }
-
-    public void setEmotion(String emotion) {
-        this.emotion = emotion;
+    public String getEmotionName() {
+        return emotionName;
     }
 
     public String getDescription() {
         return description;
     }
 
+    public LocalDate getDate() {
+        return date;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEmotionName(String emotionName) {
+        this.emotionName = emotionName;
+    }
+
     public void setDescription(String description) {
         this.description = description;
     }
 
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
+    public void setDate(LocalDate date) {
         this.date = date;
     }
 }
